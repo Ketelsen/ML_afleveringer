6f3196dd4f509b19de400dcfad6c25519176ad12
Ketelsen <andersketelsen@gmail.com>
2016-09-23 11:43:56 +0200

    PCA skelet opdateret og virkende

--- opg1.py
+++ opg1.py
@@ -45,7 +45,8 @@ b = np.zeros((398, 3))
 b[np.arange(398), a] = 1
 b = np.mat(b)
 
-#Herefter sammensættes de to matricer og slet origin
+#Herefter sammensættes de to matricer og slet origin | lad helt være med at 
+#importere origin for at springe dette step over 
 X = np.concatenate((X, b), axis=1)
 X = np.asarray(X)
 X = np.delete(X, 7, 1)
@@ -56,9 +57,17 @@ print(X[0])
 attributeNames = ['mpg','cylinders','displacement','horsepower','weight',
                   'acceleration','model year','usa','europe','japan']
 
+#Labels indstilles som modelnavne, bør nok være tresholds fra mpg
+classLabels = np.genfromtxt('../Data/mpg.txt',dtype='string',usecols=(8))
+
+classNames = sorted(set(classLabels))
+classDict = dict(zip(classNames,range(38)))
+y = np.mat([classDict[value] for value in classLabels]).T
+
 
 N = len(mpg)
 M = len(attributeNames)
+C = len(classNames)
 
 Y = X - np.ones((N,1))*X.mean(0)
 U,S,V = linalg.svd(Y,full_matrices=False)
@@ -69,3 +78,28 @@ title('Variance explained by principal components');
 xlabel('Principal component');
 ylabel('Variance explained');
 show()
+
+
+#Forsøg på plot af PCA
+V = mat(V).T
+
+# Project the centered data onto principal component space
+Z = Y * V
+
+
+# Indices of the principal components to be plotted
+i = 0
+j = 1
+
+# Plot PCA of the data
+f = figure()
+f.hold()
+title(MPG: PCA')
+Z = array(Z)
+for c in range(C):
+    # select indices belonging to class c:
+    class_mask = y.A.ravel()==c
+    plot(Z[class_mask,i], Z[class_mask,j], 'o')
+legend(classNames)
+xlabel('PC{0}'.format(i+1))
+ylabel('PC{0}'.format(j+1))
